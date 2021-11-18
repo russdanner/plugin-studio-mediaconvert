f57fd7b97f5df5891dbfd29fc0f31f615f6c387b
Russ Danner <rdanner@Russs-MacBook-Pro.local>
2021-11-18 18:16:46 -0500

    remove the example

--- authoring/plugin-sources/sources/example/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import plugin from 'sample-craftercms-plugin'
-
-const App = () => {
-  const Component = plugin.widgets['org.craftercms.sampleComponentLibraryPlugin.components.reactComponent']
-  return <Component text="John" />
-}
-
-export default App
